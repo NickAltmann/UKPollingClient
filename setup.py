3efbab24916ab8aa6f59f4373159e85638b63def
NickAltmann <nick@nickaltmann.net>
2017-05-26 20:20:04 +0100

    Setup changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='polling_client',
+      version='1.0',
+      description='UK Polling Client',
+      author='NicksTricks',
+      author_email='nick@nickaltmann.net',
+      install_requires=['pandas>=0.18.0',
+                        ]
+     )
